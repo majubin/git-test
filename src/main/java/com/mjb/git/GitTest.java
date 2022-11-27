6408494cf39c18288fcfe981b494617af0e5fb01
mjb <mjb@qq.com>
2022-11-27 08:48:34 +0800

    first commit

---
+++ src/main/java/com/mjb/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.mjb.git;
+
+/**
+ * @author majubin
+ * @create 2022-11-27-8:45
+ * @return
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
